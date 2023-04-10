f5368a27cc8b808cffe83f46db282916be220e1d
Ty Hayes <tyleonhayes@gmail.com>
2023-04-10 23:11:49 +0100

    fragile support for preventing multiple effects, and switching between multiple.  Works with Material Deck

--- scripts/module.js
+++ scripts/module.js
@@ -7,22 +7,50 @@ Hooks.once('ready', async function() {
 });
 
 
-function showVNPC(actor_id){
+function showVNPC(actor){
 	//let actor_id = "uUOzxleQwzAulwlS"
-	let actor_object = Actor.get(actor_id)
+	//let actor_object = Actor.get(actor_id)
+	if (token)
+	  {actor = token.actor}
+	let actor_object = actor
+	//let actor_object = actor
+	console.log(actor_object)
+
+	// does the actor already have an vpnc active?
+	try {
+		let existing_vnpc = await actor_object.getFlag("VNPC", "effect_ids")
+	   console.log(existing_vnpc)
+	   if (existing_vnpc != undefined){
+			console.log("existing effect - abort creation")
+			return
+		}
+	}
+	catch (err) {
+		console.log("no matching effect")
+	}
+
+	// is there another vnpc currently shown?
+	let delay = 0
+	try {
+		let existing_vnpc = await game.scenes.current.getFlag("VNPC", "active_vnpc_actor")
+	   console.log(existing_vnpc)
+	   if (existing_vnpc != undefined){
+			let dismissable_actor = await Actor.get(existing_vnpc)
+			game.macros.getName("Hide Arbitrary NPC").execute({actor: dismissable_actor})
+			console.log("existing effect - hiding other npc")
+			delay = 300
+		}
+	}
+	catch (err) {
+		console.log("no matching effect")
+	throw(err)
+	}
 
 	let art = actor_object.img
 	let name = actor_object.name
 
-	console.log(art)
-	console.log(name)
-
-
 	let randomid_img = randomID()
 	let randomid_nameplate = randomID()
-
-	console.log(randomid_img)
-	console.log(randomid_nameplate)
 
 	let flag_data = {"art_id": randomid_img, "name_id": randomid_nameplate}
 
@@ -34,8 +62,8 @@ function showVNPC(actor_id){
 		.file(art)  
 		.screenSpace()  
 		.screenSpaceAnchor({ x: 0.7, y: 0.5 })
-		.animateProperty("sprite", "position.x", {from: window.innerWidth, to: 0, gridUnits: false, duration: 700})
-		.fadeIn(500)
+		.animateProperty("sprite", "position.x", {from: window.innerWidth, to: 0, gridUnits: false, duration: 700, delay: delay})
+		.fadeIn(500, {delay: delay + 100})
 		.persist()
 		.extraEndDuration(500)
 		.origin(randomid_img)
@@ -53,20 +81,33 @@ function showVNPC(actor_id){
 		.screenSpace()  
 		.screenSpaceAnchor({ x: 0.3, y: 0.9 })
 		.text(name, text_style)
-		.animateProperty("sprite", "position.x", {from: -window.innerWidth, to: 0, gridUnits: false, duration: 700})
-		.fadeIn(500)
+		.animateProperty("sprite", "position.x", {from: -window.innerWidth, to: 0, gridUnits: false, duration: 700, delay: delay})
+		.fadeIn(500, {delay: delay})
 		.persist()
 		.extraEndDuration(500)
 		.origin(randomid_nameplate)
 	.play()
+
+	game.scenes.current.setFlag("VNPC", "active_vnpc_actor", actor_object.id)
+
+	console.log(actor_object)
 }
 
-function hideVNPC(actor_id){
-	//let actor_id = "uUOzxleQwzAulwlS"
+function hideVNPC(actor){
+		//let actor_id = "uUOzxleQwzAulwlS"
+	//let actor_object = Actor.get(actor_id)
+	if(token)
+	   {actor = token.actor}
+	let actor_object = actor
+	console.log(actor_object)
 
-let actor_object = Actor.get(actor_id)
+	let flag_data = await actor_object.getFlag("VNPC", "effect_ids")
+	console.log(flag_data)
 
-	let flag_data = actor_object.getFlag("VNPC", "effect_ids")
+	if (flag_data == undefined){
+		console.log("no current effect - aborting")
+		return
+	}
 
 	Sequencer.EffectManager.endEffects({origin: flag_data.art_id})
 	Sequencer.EffectManager.endEffects({origin: flag_data.name_id})
@@ -105,4 +146,12 @@ let actor_object = Actor.get(actor_id)
 		.text(name, text_style)
 		.animateProperty("sprite", "position.x", {to: -window.innerWidth, from: 0, gridUnits: false, duration: 700, delay: 500})
 	.play()
+
+	// is the currently active effect this npc?
+
+	let existing_vnpc = await game.scenes.current.getFlag("VNPC", "active_vnpc_actor")
+
+	if (existing_vnpc == actor_object.id){
+		game.scenes.current.unsetFlag("VNPC", "active_vnpc_actor")
+	}
 }
